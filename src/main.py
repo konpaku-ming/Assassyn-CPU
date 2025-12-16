a8a6f744bd7531ef0f8aaa71d214ba20cbb21c8c
ming <dxuanming@sjtu.edu.cn>
2025-12-16 20:58:35 +0800

    update test file

--- src/main.py
+++ src/main.py
@@ -231,7 +231,7 @@ def build_cpu(depth_log=16):
 
 if __name__ == "__main__":
     # 构建 CPU 模块
-    load_test_case("multiply")
+    load_test_case("my0to100")
     sys_builder = build_cpu(depth_log=16)
 
     circ_path = os.path.join(workspace, f"circ.txt")
