18951df3e7407952897986350f951d1ab5045300
ming <dxuanming@sjtu.edu.cn>
2025-12-17 20:36:12 +0800

    add codegen

---
+++ codegen/codegen.sh
@@ -0,0 +1,6 @@
+riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -Wl,--no-check-sections -T harvard.ld start.S mul.c -o cpu.elf
+riscv64-unknown-elf-objcopy -j .text -O binary cpu.elf icache.bin
+riscv64-unknown-elf-objcopy -j .data -j .bss -O binary cpu.elf memory.bin
+riscv64-linux-gnu-objdump -D cpu.elf > obj.exe
+hexdump -v -e '1/4 "%08x" "\n"' icache.bin > icache.hex
+hexdump -v -e '1/4 "%08x" "\n"' memory.bin > memory.hex
