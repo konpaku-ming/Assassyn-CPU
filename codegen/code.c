2c521e2350e75c98a76d9aeae69642d041d5be63
ming <dxuanming@sjtu.edu.cn>
2025-12-26 13:55:28 +0800

    update mul1to10

--- codegen/code.c
+++ codegen/code.c
@@ -1,7 +1,5 @@
 #define DATA_SIZE 10
 
-int result = 1; // 存放结果
-
 int input_1[DATA_SIZE] =
 {
    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
@@ -11,7 +9,10 @@ int input_1[DATA_SIZE] =
 // 主函数
 // =========================================================
 int main() {
+    int result; // 存放结果
     int i;
+
+    result = 1;
 
     // 向量乘法
     for (i = 0; i < DATA_SIZE; i++) {
