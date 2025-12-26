ec5f7ce57507985729d9d43655f62b756478a644
ming <dxuanming@sjtu.edu.cn>
2025-12-26 13:12:03 +0800

    add mul1to100.log

--- src/main.py
+++ src/main.py
@@ -224,7 +224,7 @@ def build_cpu(depth_log):
 
 if __name__ == "__main__":
     # 构建 CPU 模块
-    load_test_case("0to100")
+    load_test_case("mul1to10")
     sys_builder = build_cpu(depth_log=16)
 
     circ_path = os.path.join(workspace, f"circ.txt")
