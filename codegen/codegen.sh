1b30839e4a60a97abfadb15508dc3812b096b45c
ming <dxuanming@sjtu.edu.cn>
2026-01-12 21:12:44 +0800

    update codegen

--- codegen/codegen.sh
+++ codegen/codegen.sh
@@ -1,6 +1,4 @@
-riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -nostdlib -Wl,--no-check-sections -T harvard.ld start.S code.c -o cpu.elf
-riscv64-unknown-elf-objcopy -j .text -O binary cpu.elf icache.bin
-riscv64-unknown-elf-objcopy -j .data -j .bss -O binary cpu.elf memory.bin
+riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -Wl,--no-check-sections -T Neumann.ld start.S code.c -o cpu.elf
+riscv64-unknown-elf-objcopy -O binary cpu.elf memory.bin
 riscv64-linux-gnu-objdump -D cpu.elf > obj.exe
-hexdump -v -e '1/4 "%08x" "\n"' icache.bin > icache.hex
 hexdump -v -e '1/4 "%08x" "\n"' memory.bin > memory.hex
